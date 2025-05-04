e6127441def26429e47aab3fca19990224baecc0
Diego Alvarez <dalvarez@bitz.pe>
2025-05-04 17:18:05 -0500

    feat(jest reporter): added html report

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,17 @@
 export default {
     preset: "ts-jest",
     testMatch: ["**/*.test.ts"],
+    reporters: [
+        "default",
+        [
+            "./node_modules/jest-html-reporter",
+            {
+                pageTitle: "Test Report",
+                includeFailureMsg: true,
+                inclugeConsoleLog: true,
+                sort: "titleAsc",
+            },
+        ],
+    ],
     setupFiles: ["<rootDir>/jest.setup.ts"],
 };
